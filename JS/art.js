a929fc2e87a338ed53355df0006a1d9781160cd1
Ruehl1113 <ruehlluna@gmail.com>
2024-12-19 01:39:23 -0600

    Add files via upload

---
+++ JS/art.js
@@ -0,0 +1,20 @@
+const paragraphs = document.querySelectorAll(".section_paragraph");
+
+document.addEventListener("scroll", function () {
+    paragraphs.forEach((paragraph) => {
+        if (isInView(paragraph)) {
+            paragraph.classList.add("section_paragraph--visible");
+        }
+    });
+});
+
+
+function isInView(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.bottom > 0 && rect.top < 
+        (window.innerHeight - 100 || document.documentElement.clientHeight - 100)
+    );
+}
+
+
